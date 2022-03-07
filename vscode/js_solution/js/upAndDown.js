7b8fbbbeae0d4e3b4c0bedf071bb975b73ec1346
Seokho <32010878+seokho94@users.noreply.github.com>
2022-03-07 12:18:12 +0900

    Up & Down Game
    finish 기능 구현

--- vscode/js_solution/js/upAndDown.js
+++ vscode/js_solution/js/upAndDown.js
@@ -1,37 +1,44 @@
 //요소 선택 과정
 var unknown = document.querySelector("#targetNumber");
+var checkNumber = document.querySelector("#check_result");
 var sendBtn = document.getElementById("sendData");
 var resetBtn = document.getElementById("resetData");
+var userChoice = document.getElementById("textData");
 
 var target = Math.floor(Math.random()*100)+1;;
-var result = target;
+var result = "???";
+checkNumber.textContent = '숫자를 입력해주세요.';
 
 setNumber(result);
-upNdown();
 sendBtn.onclick = sendNumber;
 resetBtn.onclick = resetNumber;
 
-function setNumber(string){
-    unknown.textContent = result;
+function setNumber(str){
+    unknown.textContent = str;
 }
 //Up & Down 알고리즘
 function upNdown(number){
-    // if(userNumber < target) alert("up");
-    // else if(userNumber > target) alert("down");
-    // else {
-    //     alert("맞혔습니다.");
-    //     result = target;
-    //     unknown.textContent = result;
-    //     break;
-    // }
+    userNumber = number;
+    if(userNumber < target) {
+        checkNumber.textContent = 'UP!';
+    }
+    else if(userNumber > target) {
+        checkNumber.textContent = 'DOWN!';
+    }
+    else {
+        checkNumber.textContent = '정답입니다!';
+        setNumber(target);
+    }
 }
 
 
 
 //서브밋 클릭시 input data 전송
 function sendNumber() {
-    var userData = document.getElementById("#textData");
+    console.log("sendNumber 실행");
+    var userData = userChoice.value
     upNdown(userData);
+    userChoice.value="";
 }
 
 //리셋버튼 클릭시 랜덤 숫자 초기화
