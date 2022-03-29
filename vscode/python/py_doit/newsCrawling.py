c34feda1656b4bde85669a1d1ee0e72273ed1a72
seokho94 <happt7889@gmail.com>
2022-03-29 14:14:21 +0900

    Web Crawling
    인터넷 기사 모으기

--- vscode/python/py_doit/newsCrawling.py
+++ vscode/python/py_doit/newsCrawling.py
@@ -11,3 +11,52 @@ for i in soup.find_all('div', {"class" : "item_issue"}) :
 
 article1 = 'https://news.v.daum.net/v/20220329102612815'
 soup2 = bs(ur.urlopen(article1).read(), 'html.parser')
+
+for i in soup2.find_all('p') :
+    print(i.text)
+    
+headline = soup.find_all('div', {"class" : "item_issue"})
+
+print(headline[0].text)
+
+for i in headline :
+    print(i.text, '\n')
+    
+    soup3 = bs(ur.urlopen(i.find_all('a')[0].get('href')).read(),'html.parser')
+    
+    for j in soup3.find_all('p') :
+        print(j.text)
+        
+f = open('links.txt', 'w')
+for i in soup.find_all('div', {"class" : "item_issue"}) :
+    f.write(i.find_all('a')[0].get('href')+'\n')
+    
+f.close()
+
+article1 = 'https://news.v.daum.net/v/20220329122101265'
+
+soup = bs(ur.urlopen(article1).read(), 'html.parser')
+
+f=open('article_1.txt', 'w', encoding='utf-8')
+
+for i in soup.find_all('p') :
+    f.write(i.text)
+    
+f.close()
+
+url = 'https://news.v.daum.net/v/20220329111302294'
+soup = bs(ur.urlopen(url).read(),'html.parser')
+f = open('article_total.txt','w')
+for i in soup.find_all('div',{"class" : "item_issue"}) :
+    try :
+        f.write(i.text + '\n')
+        f.write(i.find_all('a')[0].get('href') + '\n')
+        soup2 = bs(ur.urlopen(i.find_all)[0].get('href').read(),'html.parser')
+        
+        for j in soup2.find_all('p') :
+            f.write(j.text)
+        
+    except :
+        pass
+f.close()
+
