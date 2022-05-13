2280e72e132eb1b5f4014d80f33e75ac581c9162
seokho94 <happt7889@gmail.com>
2022-05-13 10:25:05 +0900

    backup

--- cointrading/main.py
+++ cointrading/main.py
@@ -1,28 +1,42 @@
 import sys
+import py
+import pyupbit as upbit
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
-import Current_price
+##from Current_price import total
 
-ui = uic.loadUi('coingui.Ui')[0]
-class MainWindow(QWidget, ui):
+
+ui = uic.loadUiType('coingui.Ui')[0]
+class MainWindow(QMainWindow, ui):
   def __init__ (self):
     super().__init__()
     self.setupUi(self)
     self.setWindowTitle("CoinTrding")
-  def show(self):
-    data = [1,2,3,4]
-    self.QTableWidget.setItem(1,1,QTableWidgetItem(str(data[0])))
-    
-"""
-class QL(QGridLayout):
-  def __init__(self):
-    super().__init__()
-    self.ui = uic.loadUi("coingui.ui",self)
-    self.show()
-  def show(self):
-    data = [1,2,3,4]
-    self.tableWidget_3.setItem(1,1,QTableWidgetItem(str(data[0])))
-"""
+class QV(QGridLayout):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(2)
+        self.tableWidget.setColumnCount(2)
+
+        self.tableWidget.setItem(0, 0, QTableWidgetItem('item0'))
+        self.tableWidget.setItem(0, 1, QTableWidgetItem('item1'))
+        self.tableWidget.setItem(1, 0, QTableWidgetItem('item2'))
+        self.tableWidget.setItem(1, 1, QTableWidgetItem('item3'))
+        self.tableWidget.setText
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.tableWidget)
+        self.setLayout(layout)
+
+        self.setWindowTitle('PyQt5 - QTableWidget')
+        self.setGeometry(300, 100, 600, 400)
+        self.show()
+
   
 if __name__ == "__main__":
   app = QtWidgets.QApplication(sys.argv)
