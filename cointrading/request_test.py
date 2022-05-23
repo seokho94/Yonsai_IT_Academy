6ad41e2eef4eb1883f53a61cc051511f31579afb
seokho94 <happt7889@gmail.com>
2022-05-23 12:03:17 +0900

    request fucntion & thread test

---
+++ cointrading/request_test.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+import pyupbit
+import threading
+import time
+
+def request_data(function, ticker, interval='minute1', to=datetime.today().strftime('%Y-%m-%d'), count = 1) :
+    data = []
+    if function == 'current' :
+        price = current_price(ticker)
+        return price
+    elif function == 'order' :
+        data = order_book(ticker)
+        return data
+    elif function == 'ohlcv' :
+        data = ohlcv(ticker, interval, to, count)
+        return data
+
+def current_price(ticker) :
+    price = pyupbit.get_current_price(ticker)
+    return price
+
+def order_book(ticker) :
+    orderbook = pyupbit.get_orderbook(ticker)
+    return orderbook
+
+def ohlcv(ticker, interval, to, count) :
+    data = pyupbit.get_ohlcv(ticker = ticker, interval = interval, count = count, to = to)
+    return data
+
+
+def thread_1() :
+    print('Thread_1 Start')
+    request_data('current', 'KRW-BTC')
+    time.sleep(1)
+    thread1.run()
+    
+def thread_2() :
+    print('Thread_2 Start')
+    request_data('order', 'KRW-BTC')
+    time.sleep(1)
+    thread2.run()
+    
+def thread_3() :
+    print('Thread_3 Start')
+    request_data('ohlcv', 'KRW-BTC', 'minute1', count = 10)
+    time.sleep(1)
+    thread3.run()
+    
+    
+thread1 = threading.Timer(1, thread_1)
+thread2 = threading.Timer(1, thread_2)
+thread3 = threading.Timer(1, thread_3)
+
+thread1.start()
+thread2.start()
+thread3.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+
+# def thread_run():
+#     global count 
+#     total()
+#     print(output_data)
+#     count = count + 1
+#     update = threading.Timer(3, thread_run)
+#     if(count>=50) : update.cancel()
+#     update.start()
--- cointrading/seebarcoin.py
+++ cointrading/seebarcoin.py
@@ -137,7 +137,41 @@ class MyWindow(QMainWindow, ui):
         ##self.setLayout(self.current_verlayout)
         ##self.update_curPrice()
         
-        
+    def hogaUI(self):
+        for i in range(self.tableBids.rowCount()):
+            # 매도호가
+            item_0 = QTableWidgetItem(str(""))
+            item_0.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableAsks.setItem(i, 0, item_0)
+ 
+            item_1 = QTableWidgetItem(str(""))
+            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableAsks.setItem(i, 1, item_1)
+ 
+            item_2 = QProgressBar(self.tableAsks)
+            item_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_2.setStyleSheet("""
+                 QProgressBar {background-color : rgba(0, 0, 0, 0%);border : 1}
+                 QProgressBar::Chunk {background-color : rgba(255, 0, 0, 50%);border : 1}
+             """)
+            self.tableAsks.setCellWidget(i, 2, item_2)
+ 
+            # 매수호가
+            item_0 = QTableWidgetItem(str(""))
+            item_0.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableBids.setItem(i, 0, item_0)
+ 
+            item_1 = QTableWidgetItem(str(""))
+            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableBids.setItem(i, 1, item_1)
+ 
+            item_2 = QProgressBar(self.tableBids)
+            item_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_2.setStyleSheet("""
+                QProgressBar {background-color : rgba(0, 0, 0, 0%);border : 1}
+                QProgressBar::Chunk {background-color : rgba(0, 255, 0, 40%);border : 1} 
+             """)
+            self.tableBids.setCellWidget(i, 2, item_2)
 
 
 if __name__ == "__main__":
